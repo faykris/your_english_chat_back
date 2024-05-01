a8efa5dbc72f4e1cc4fe4f8446ee08b7acdbdd2b
faykris <faykris28@gmail.com>
2024-05-01 11:28:05 -0500

    Add cors and fix JWT Values

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,14 +4,15 @@ import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
+require('dotenv').config();
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({
       global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+      secret: `${process.env.JWT_SECRET}`,
+      signOptions: { expiresIn: `${process.env.JWT_EXPIRATION}` },
     }),
   ],
   providers: [AuthService, UserService],
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   await app.listen(3002);
 }
 bootstrap();
